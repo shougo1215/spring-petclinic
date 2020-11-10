eefd9d90ca8766ee9ba5d0da67c503fa4504a72a
shougo1215 <shougoyamada01@gmail.com>
2020-11-10 01:55:59 +0000

    message

--- ex6-5.js
+++ ex6-5.js
@@ -1,1 +1,3 @@
-var who65 = 'world';function sayhello65() {alert('Hello, ' + who65 + '!');}function taro65() {who = '太郎';
+var who65 = 'world';
+function sayhello65() {alert('Hello, ' + who65 + '!');}
+function taro65() {who = '太郎';}
