27ddf1ac537a2b75958b35acf5e2438cccb4b300
shougo1215 <shougoyamada01@gmail.com>
2020-12-01 02:18:37 +0000

    message

--- ex07-1.js
+++ ex07-1.js
@@ -1,4 +1,3 @@
 function sayhello71() {
  alert('Hello, ' + 
-document.getElementById(‘who71').value + '!');
-}
+document.getElementById(‘who71').value + '!');}
--- ex6-5.js
+++ ex6-5.js
@@ -1,4 +1,4 @@
 var who65 = 'world';
 function sayhello65() {alert('Hello, ' + who65 + '!');}
-function taro65() {who = '太郎';}
-function hanako65() {who = '花子';}
+function taro65() {who65 = '太郎';}
+function hanako65() {who65 = '花子';}
