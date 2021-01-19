e7afd42a2c92a3001a51a8f22a4da8de3a9fe048
shougo1215 <shougoyamada01@gmail.com>
2021-01-19 02:18:26 +0000

    message

--- ex06-9.js
+++ ex06-9.js
@@ -2,5 +2,5 @@ function txtchange1(){
     document.getElementById('txt1').innerHTML= 'ナンバーワン！';
 }
 function imagechange1(){
-    document.getElementById("logo").src= "./a/image.jpg";
+    document.getElementById("logo").src= "./a/Untitled/download-1.png";
 }
